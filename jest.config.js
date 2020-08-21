bfac60fd2d3c34282a0976e24ade91394032b307
Nathan Torquato <n.torquato.silva@gmail.com>
2020-08-21 10:02:50 -0300

    fix: use correct paths for showing jest what files to collect coverage from

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,9 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    // '!<rootDir>/src/**/*-protocols.ts',
-    // '!/**/protocols/**.ts'
+    '!<rootDir>/src/domain/**/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!<rootDir>/**/protocols/**.ts',
   ],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
